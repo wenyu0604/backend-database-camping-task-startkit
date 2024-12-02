8876a31ea9475877c1a073bb9f90c8d90a4bd358
C.C <coco2231021@gmail.com>
2024-12-03 00:10:41 +0800

    update1-1

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -15,8 +15,9 @@
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
 INSERT INTO
-"USER"('李燕容', 'lee2000@hexschooltest.io', 'USER'),
+"USER"(name, email, role)
 VALUES
+('李燕容', 'lee2000@hexschooltest.io', 'USER'),
 ('王小明', 'wXlTq@hexschooltest.io', 'USER'),
 ('肌肉棒子','muscle@hexschooltest.io', 'USER'),
 ('好野人', 'richman@hexschooltest.io', 'USER'),
